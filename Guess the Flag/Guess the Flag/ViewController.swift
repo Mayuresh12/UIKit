13f34a30204fc28afcc2e066b4137fe4e9863213
MRAO0212 <mayureshvrao@gmail.com>
2020-09-28 00:17:50 -0700

    add the flags

---
+++ Guess the Flag/Guess the Flag/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  Guess the Flag
+//
+//  Created by Mayuresh Rao on 9/27/20.
+//  Copyright © 2020 Mayuresh Rao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var button1: UIButton!
+    @IBOutlet var button2: UIButton!
+    @IBOutlet var button3: UIButton!
+    
+    var countries = [String]()
+    var score = 0
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        countries += ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
+        askQuestion()
+    }
+
+    func askQuestion() {
+        button1.setImage(UIImage(named: countries[0]), for: .normal)
+        button2.setImage(UIImage(named: countries[1]), for: .normal)
+        button3.setImage(UIImage(named: countries[2]), for: .normal)
+    }
+    
+}
+
