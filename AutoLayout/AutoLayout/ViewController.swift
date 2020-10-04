2010007fb7156c0eeeb433ee042dd3f3da5bfcfe
MRAO0212 <mayureshvrao@gmail.com>
2020-10-03 23:44:25 -0700

    Autolayout project

---
+++ AutoLayout/AutoLayout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AutoLayout
+//
+//  Created by Mayuresh Rao on 10/3/20.
+//  Copyright © 2020 Mayuresh Rao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
